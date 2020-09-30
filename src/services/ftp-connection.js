19cfa20d7c7402fa73ce9ca8a79ce50122556a3e
Tiago Vidal <tiagod3p@gmail.com>
2020-09-30 08:27:26 -0300

    feat: connect with ftp server

---
+++ src/services/ftp-connection.js
@@ -0,0 +1,21 @@
+import ftp from 'basic-ftp'
+
+const ftpConnection = async () => {
+  try {
+    const client = new ftp.Client()
+    client.ftp.verbose = true
+
+    await client.access({
+    host: process.env.HOST,
+    user: process.env.USER,
+    password: process.env.PASSWORD,
+    secure: false,
+    })
+
+    return client
+  } catch(err) {
+    console.error(err.stack)
+  }
+}
+
+ftpConnection()
